7eb019d58d5124c69c99b40871a4aaeb2f6ecb1b
gk7734 <gamekiller7734@naver.com>
2023-07-31 01:55:14 +0900

    use to state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Todo from "./components/Todo";
 
 function App() {
-    return <div>
-        <h1>My Todos</h1>
-        <Todo text='Learn React' />
-        <Todo text='Learn Next.js' />
-        <Todo text='Learn Vue.js' />
-    </div>;
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <Todo text='Learn React' />
+            <Todo text='Learn Next.js' />
+            <Todo text='Learn Vue.js' />
+        </div>
+    );
 }
 
 export default App;
