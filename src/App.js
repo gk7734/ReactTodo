a300279eb77385bbd64141e8e164776bbd1b6ac0
gk7734 <gamekiller7734@naver.com>
2023-07-31 00:50:41 +0900

    Todo Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>
-    <h1>My Todos</h1>
-    <div className='card'>
-      <h2>Title</h2>
-      <div className='actions'>
-        <button className='btn'>Delete</button>
-      </div>
-    </div>
-  </div>;
+    return <div>
+        <h1>My Todos</h1>
+        <Todo text='Learn React' />
+        <Todo text='Learn Next.js' />
+        <Todo text='Learn Vue.js' />
+    </div>;
 }
 
 export default App;
